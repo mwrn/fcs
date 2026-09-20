0b6dcdf03853dbc23f00b18fcb730299d541ecd3
mwrn <abdikadar@gmail.com>
2026-09-20 06:12:38 -0700

    domain

--- oceanstars_project/settings.py
+++ oceanstars_project/settings.py
@@ -38,9 +38,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-05qej-9dbjlk*0)nj@zx^^o4kt*bd(==&3=m6-w50$-n4re$q2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# FIXED: Removed 'https://' prefix because Django expects domains or IPs here only
+ALLOWED_HOSTS = [
+    'fcs-production.up.railway.app', 
+    
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    
+    'https://fcs-production.up.railway.app'
+]
+
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
